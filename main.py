d7b809b6e83beb6832cee8279f53df933a3c9021
ssw1426 <80308398+ssw1426@users.noreply.github.com>
2021-03-15 16:10:30 +0900

    Update main.py

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ async def on_message(message):
                 amount = message.content[4:]
                 await message.delete()
                 await message.channel.purge(limit=int(amount))
-                message = await message.channel.send(embed=discord.Embed(title='[ Delete Massage ]' + str(amount) + '개의 메시지가 삭제되었습니다', colour=discord.Colour.green()))
+                message = await message.channel.send(embed=discord.Embed(title='```[ Delete Massage ]```' + str(amount) + '개의 메시지가 삭제되었습니다', colour=discord.Colour.green()))
                 await asyncio.sleep(2)
                 await message.delete()
             else:
